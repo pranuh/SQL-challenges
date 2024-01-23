46b12d74de1c765bb7e5969d38143cb48af6006f
pranay chandra <63705514+pranuh@users.noreply.github.com>
2024-01-23 21:37:15 +0530

    Update Laptop vs. Mobile Viewership.sql

--- Datalemur/Laptop vs. Mobile Viewership.sql
+++ Datalemur/Laptop vs. Mobile Viewership.sql
@@ -1,1 +1,10 @@
-https://datalemur.com/questions/laptop-mobile-viewership
+[s](https://datalemur.com/questions/laptop-mobile-viewership)
+
+-- Calculating the total number of views for 'laptop' devices
+SELECT 
+  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+  -- Calculating the total number of views for 'tablet' and 'phone' devices
+  SUM(CASE WHEN device_type IN ('tablet','phone') THEN 1 ELSE 0 END) AS mobile_views
+FROM 
+  viewership;
+
