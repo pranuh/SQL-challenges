eac9e1bd6f1e7b53872f33a5b2a2ade6d2554e7d
pranay chandra <63705514+pranuh@users.noreply.github.com>
2024-01-25 22:59:41 +0530

    Create Average Review Ratings.sql

---
+++ Datalemur/Average Review Ratings.sql
@@ -0,0 +1,17 @@
+-- Journal Entry for Code Revision
+
+-- Goal: Calculate the average stars given in reviews per month and product.
+
+-- Selecting the month, product_id, and rounding the average stars to two decimal places
+SELECT 
+    EXTRACT(month FROM submit_date) as mth,
+    product_id,
+    ROUND(AVG(stars), 2) as avg_stars 
+FROM 
+    reviews
+-- Grouping the result by month and product_id
+GROUP BY 
+    1, 2
+-- Ordering the result by month and product_id
+ORDER BY 
+    1, 2;
