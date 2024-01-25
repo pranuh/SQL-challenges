a77690df786df9227c65ce30e201bbae53a5edb7
pranay chandra <63705514+pranuh@users.noreply.github.com>
2024-01-25 22:52:35 +0530

    Update teams-power-users.sql

--- Datalemur/teams-power-users.sql
+++ Datalemur/teams-power-users.sql
@@ -1,1 +1,21 @@
 https://datalemur.com/questions/teams-power-users
+
+
+-- Selecting sender_id and counting the number of messages for each sender in August 2022
+SELECT 
+    sender_id,
+    COUNT(message_id) as msg_count 
+FROM 
+    messages
+-- Filtering messages sent in August 2022
+WHERE 
+    EXTRACT(month FROM sent_date) = 8
+    AND EXTRACT(year FROM sent_date) = 2022
+-- Grouping the result by sender_id
+GROUP BY 
+    sender_id 
+-- Ordering the result by msg_count in descending order
+ORDER BY 
+    msg_count DESC
+-- Limiting the result to the top 2 senders
+LIMIT 2;
